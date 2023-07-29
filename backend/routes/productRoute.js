a0cb5c4a2c85d57ddbf0696fed52bdedd23072f1
AVINASH SINGH <avinashsingh93690@gmail.com>
2023-07-30 03:35:00 +0530

    review work done

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const {isAuthenticatedUser,authorizeRoles} = require("../middleware/auth");
-const { getAllProducts,createProduct,updateProduct,deleteProduct,getProductDetails } = require("../controllers/productController");
+const { getAllProducts,
+        createProduct,
+        updateProduct,
+        deleteProduct,
+        getProductDetails,
+        createProductReview,
+        getProductReviews,
+        deleteReview
+ } = require("../controllers/productController");
 
 const router = express.Router();
 
@@ -9,7 +17,9 @@ router.route("/products").get(getAllProducts)
 router.route("/products/:id").get(getProductDetails)
 router.route("admin/products/:id").put(isAuthenticatedUser,authorizeRoles("admin"),updateProduct)
 router.route("admin/products/:id").delete(isAuthenticatedUser,authorizeRoles("admin"),deleteProduct)
-
+router.route("/review").put(isAuthenticatedUser, createProductReview);
+router.route("/reviews").get(getProductReviews)
+router.route("/reviews").delete(isAuthenticatedUser, deleteReview);
 
 
 module.exports=router
